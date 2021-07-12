ac938f8c36392d2abae35fc34429ebdc6fd24732
Manoj Jayapprakash <45529406+manoj-jayapprakash@users.noreply.github.com>
2021-07-12 13:47:32 +0200

    Uploading initial setup of the project (#1)
    * Uploading initial setup of the project
    
    * Resolved errors about the path
    
    * Change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+// import { Switch, Route } from 'react-router-dom';
+// import { Homepage } from './pages/Homepage';
+// import { Search } from './pages/Search';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
       App Placeholder
+      <Header />
+      {/* <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch> */}
     </div>
   );
 }
