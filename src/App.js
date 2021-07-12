fe086ad7c41f707413b0dc7ccbc06d804df4be81
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-07-12 14:04:25 +0530

    Uploading initial setup of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Homepage } from './pages/Homepage';
+import { Search } from './pages/Search';
 
 function App() {
   return (
     <div>
       App Placeholder
+      <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
